26e67a58a1ef59639c3d9f261b9ee7ab1e097bbf
Bruce Lin <brucelin1218@gmail.com>
2014-05-13 17:05:50 -0700

    Add search, and adjust footer. Add script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	$('.menuButton').hover(function(){
+		$(this).addClass('active');
+	},
+	$('.menuButton').(function(){
+		$(this).removeClass('active');
+	});
+    
+    $('.search').focus(function(){
+      ('input').css('outline-color', '#FF0000');
+    });
+
+
+    var input = $('input[name=search]').var();
+
+
+
+});
