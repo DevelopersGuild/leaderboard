34a435d0fdb4f65535a732b410833f1158a51016
Bruce Lin <brucelin1218@gmail.com>
2014-05-20 11:13:11 -0700

    Fix all the buttons, use <div> to replace <button>, apply jQuery to buttons, fix the postion of icons and names in the list.

--- public/js/script.js
+++ public/js/script.js
@@ -1,19 +1,22 @@
-$(document).ready(function(){
-	$('.menuButton').hover(function(){
-		$(this).addClass('active');
-	},
-	$('.menuButton').(function(){
-		$(this).removeClass('active');
-	});
-    
-    $('.search').focus(function(){
-      ('input').css('outline-color', '#FF0000');
-    });
-
-
-    var input = $('input[name=search]').var();
-
-
-    
-
+$('.topbutton').hover(function(){
+	$(this).addClass('active');
+}, function(){
+	$(this).removeClass('active');
 });
+
+
+
+$('.groupbutton').hover(function(){
+    $(this).addClass('active1');
+}, function(){
+    $(this).removeClass('active1');
+});
+
+
+
+
+$('.search').focus(function() {
+  ('input').css('outline-color', '#FF0000');
+});
+
+//var input = $('input[name=search]').var();
